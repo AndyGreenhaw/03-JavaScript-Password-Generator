dfbf550fcd09b741b46510d2da60d84b595568f8
Andy Greenhaw <andy.greenhaw@gmail.com>
2020-09-27 20:29:18 -0600

    Boom! This is an extremely clean code from what I can tell. Just need to write my read me.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 //Goal
 var password;
+var passwordOnScreen;
 
 // Action
 var ready;
@@ -37,32 +38,31 @@ var possibleSpecial = "!@#$%^&*";
 
 function generateLowerCase(){
   lowercaseData = possibleLowerCase.charAt(Math.floor(Math.random() * 25) + 1);
-  console.log("Function Result: " + lowercaseData);
+  console.log("Lowercase Result: " + lowercaseData);
 };
 
 function generateUpperCase(){
   uppercaseData = possibleUpperCase.charAt(Math.floor(Math.random() * 25) + 1);
-  console.log("Function Result: " + uppercaseData);
+  console.log("Uppercase Result: " + uppercaseData);
 };
 
 function generateNumber(){
   numberData = (Math.floor(Math.random() * 9) + 1);
-    console.log("Function Result: " + numberData);
+  console.log("Number Result: " + numberData);
 };
 
 function generateSpecial(){
   specialData = possibleSpecial.charAt(Math.floor(Math.random() * 7) + 1);
-  console.log("Function Result: " + specialData);
+  console.log("Special Result: " + specialData);
 };
 
 //................................................//
 //.....................START......................//
 //................................................//
 
-// User Clicks "Generate Password" Button: Starts readyToStart Function 
+// User Clicks "Generate Password" Button to Start the first function. 
 generateBtn.addEventListener("click", readyToStart); 
 
-// Start readyToStart() Function
 function readyToStart() {
 
   // Confirm whether user is ready
@@ -75,15 +75,13 @@ function readyToStart() {
 };
 
 // Collect Password Length
-
 function collectPasswordLength(){
 
   // Ask User for Password Length
     passwordLength = prompt("How many characters does your password need to be?");
 
-    // Asks user to try again if user entry is not a number.
+    // Ask user to try again if user entry is not a number.
     if (isNaN(passwordLength)) {
-
       tryAgain = confirm("That's not a number. Try again?");
         if (tryAgain){
             collectPasswordLength(); 
@@ -91,7 +89,7 @@ function collectPasswordLength(){
           alert("Goodbye!"); 
         }
 
-    // Asks user to try again if user enters nothing or 0.
+    // Ask user to try again if user enters nothing or 0.
       } else if (passwordLength == 0){
         tryAgain = confirm("You need more characters than 0. Try again?");
         if (tryAgain){
@@ -102,24 +100,26 @@ function collectPasswordLength(){
 
     // If user clicks "Cancel," stop program.
     } else if (passwordLength === false){
-      alert("Goodbye"); 
+        alert("Goodbye"); 
   
-    // If user enters a number, start collectCharacterData function.
+    // If user enters a valid number, start collecting password character data.
     } else {
-      collectCharacterData(passwordLength);
+        collectCharacterData(passwordLength);
   }
 };
 
-// Starts Collecting Character Data
+// Collect Password Character Data
 
 function collectCharacterData(){
-  console.log(passwordLength); //GOOD
+  console.log(passwordLength);
   
+  //Ask User What Characters They Want
   lowercaseAnswer = confirm("Does your password require lowercase letters?");
   uppercaseAnswer = confirm("Does your password require uppercase letters?");
   numbersAnswer = confirm("Does your password require numbers?");
   specialAnswer = confirm ("Does your password require special characters");
 
+  //Go Through Each Answer and Push Desired Variables to passwordVariables Array.
   if (passwordVariables.length < passwordLength) {
 
   for (var i = 0; i <= passwordLength; i++){
@@ -128,113 +128,50 @@ function collectCharacterData(){
       if (lowercaseAnswer === true){ 
         generateLowerCase();
         passwordVariables.push(lowercaseData);
-        console.log(lowercaseData);
         };
         
       //UpperCase Generator
       if (uppercaseAnswer === true) {
         generateUpperCase();
         passwordVariables.push(uppercaseData);
-        console.log(uppercaseData);
       };
 
       //Numbers Generator
       if (numbersAnswer === true) {
         generateNumber();
         passwordVariables.push(numberData);
-        console.log(numberData);   
       };
 
       //Special Characters Generator
       if (specialAnswer === true) {
         generateSpecial();
         passwordVariables.push(specialData);
-        console.log(specialData);
       };
-
     };
     
-    if (passwordVariables.length = passwordLength){
-        generatePassword();  
-    };
+      // If passwordVariables Length Goes Over Desired Length, Remove Character from Beginning.
+      if (passwordVariables.length > passwordLength){
+          passwordVariables.shift();
+        }
+
+      // After "passwordVariables" Array Reaches Desired Length, Move On to Generate Password.
+      if (passwordVariables.length = passwordLength){
+          generatePassword();  
+        };
   };
-      console.log("VARIABLE ARRAY: " + passwordVariables);
-      console.log("ARRAY LENGTH: " + passwordVariables.length);
-};
-      
+  console.log("VARIABLE ARRAY: " + passwordVariables);
+  console.log("ARRAY LENGTH: " + passwordVariables.length);
+};      
 
-// Random Function Generator
-
-//function generateRandom() {
-
-  //if (lowercaseAnswer === true){ 
-    //lowercaseRandom = possibleLowerCase.charAt(Math.floor(Math.random() * 26) + 1);
-    //console.log("RANDOM: " + lowercaseRandom);
-    //randomFunctions.push(lowercaseRandom);
-    //};
-    
-  //if (uppercaseAnswer === true) {
-    //uppercaseRandom = possibleUpperCase.charAt(Math.floor(Math.random() * 26) + 1);
-    //console.log("RANDOM: " + uppercaseRandom);
-    //randomFunctions.push(uppercaseRandom);
-  //};
-
-  //if (numbersAnswer === true) {
-    //numberRandom = (Math.floor(Math.random() * 9) + 1);
-    //console.log("RANDOM: " + numberRandom);
-    //randomFunctions.push(numberRandom);
-  //};
-
-  //if (specialAnswer === true) {
-    //specialRandom = possibleSpecial.charAt((Math.floor(Math.random()) * 9) + 1);
-    //console.log("RANDOM: " + specialRandom); 
-    //randomFunctions.push(specialRandom);
-  //};
-
-  //console.log("RANDOM ARRAY: " + randomFunctions);
-  //console.log("RANDOM ARRAY LENGTH: " + randomFunctions.length);
-
-  //for (var i = 0; i < randomFunctions.length; i++){
-    //passwordVariables.push(randomFunctions[i]);
-    //console.log("TEST " + randomChar);
-  //};
-
-  //if (passwordVariables.length < passwordLength){
-    //for (var i =0; i < passwordLength; i++){
-    //addonPosition = (Math.floor(Math.random() * passwordVariables.length +1));
-    //addonChar = passwordVariables[Math.floor(Math.random() * 8)];
-    //passwordVariables.push(addonChar);
-    //};
-  //}
-
-  //generatePassword();
-
-//};
-// Complete Password Function
-
+// Generate Password
 function generatePassword(){
 
+// Assembles passwordVariable Array Single Password String and Return Password to User.
   password = passwordVariables.join("");
     console.log("FINAL PASSWORD: " + password);
-    console.log("LENGTH: " + password.length)
+    console.log("PASSWORD LENGTH: " + password.length)
     console.log(passwordVariables.length);
-    alert("Your new password is " + password + " Congratulations.")
-
+    alert("Your new password is " + password + ". Congratulations!")
+    passwordText.setAttribute("placeholder", "Your new password is " + password + " Congratulations.") 
+    
 };
-
-
-      //var pw0ptions = {
-        //passwordLength,
-        //useLowerCase: lowercaseAnswer, 
-        //useUpperCase: uppercaseAnswer,
-        //useNumbers: numbersAnswer,
-        //useSpecial: specialAnswer,
-        //}      
-
-  //passwordText = document.querySelector("#password");
-
-    //if(sure=true){
-    //alert("Your password is" + password);
-    //} else {
-    //alert("Thank you for wasting our time.");
-    //}
